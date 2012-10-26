7772cf605c356169e3120323b7c33f10b00278e7
ZequeZ <zequez@gmail.com>
2012-10-26 11:00:08 -0700

    Use already configured Spree Amazon S3 instead of reading the configuration from different sources

--- app/models/spree/slide.rb
+++ app/models/spree/slide.rb
@@ -1,33 +1,26 @@
 module Spree
   class Slide < ActiveRecord::Base
 
+    URL_PATH = "spree/showcase/:id/:style/:basename.:extension"
+
     has_attached_file :image,
       :styles=>{:thumb=> ["#{SpreeShowcase::Config.thumbnail_style}"],
                 :showcase=> ["#{SpreeShowcase::Config.showcase_style}"]},
-      :url => '/spree/showcase/:id/:style/:basename.:extension',
-      :path => ':rails_root/public/spree/showcase/:id/:style/:basename.:extension'
-    
-    # Add S3 and Heroku support
-    s3_options = if ENV['S3_KEY'] && ENV['S3_SECRET'] && ENV['S3_BUCKET']
-      {
-        :storage => 's3',
-        :s3_credentials => {
-          :access_key_id     => ENV['S3_KEY'],
-          :secret_access_key => ENV['S3_SECRET']
-        },
-        :bucket => ENV['S3_BUCKET']
-      }
-    elsif (s3_config_file = Rails.root.join('config','s3.yml')).exist?
-      {
-        :storage => 's3',
-        :s3_credentials => s3_config_file
-      }
-    else
-      { :storage => 'filesystem' }
+      :default_style => :showcase,
+      :path => ":rails_root/public/#{URL_PATH}",
+      :url => "/#{URL_PATH}"
+
+    # Use the already configured S3 options in Spree
+    if Spree::Config[:use_s3]
+      attachment       = Spree::Slide.attachment_definitions[:image]
+      spree_attachment = Spree::Image.attachment_definitions[:attachment]
+
+      attachment.reverse_merge! spree_attachment
+
+      attachment[:path] = URL_PATH
+      attachment[:url]  = spree_attachment[:url]
     end
 
-    attachment_definitions[:image] = (attachment_definitions[:image] || {}).merge(s3_options)
-    
     default_scope order(:position) # Slides should always be ordered by position specified by user.
     scope :published, where(:published=>true)
     scope :no_slides, lambda {|num| limit(num)}
